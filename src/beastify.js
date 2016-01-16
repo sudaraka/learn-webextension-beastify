e4c54b9070ee3d0868c67c11161030472bceaed2
Sudaraka Wijesinghe <sudaraka@sudaraka.org>
2016-01-16 12:26:28 +0530

    Switching with 2 space indentation for .js files

--- src/beastify.js
+++ src/beastify.js
@@ -1,31 +1,32 @@
 (() => {
-    'use strict';
+  'use strict';
 
-    const
-        handleMessage = (msg /* , sender, sendResponse */) => {
-            const beastURL = chrome.extension.getURL(msg.beast);
+  const
+    handleMessage = (msg /* , sender, sendResponse */) => {
+      const beastURL = chrome.extension.getURL(msg.beast);
 
-            insertImage(beastURL);
+      insertImage(beastURL);
 
-            chrome.runtime.onMessage.removeListener(handleMessage);
-        },
+      chrome.runtime.onMessage.removeListener(handleMessage);
+    },
 
-        insertImage = (url) => {
-            const newImg = document.createElement('img');
+    insertImage = (url) => {
+      const newImg = document.createElement('img');
 
-            newImg.className = 'beastify-image';
-            newImg.setAttribute('src', url);
-            newImg.setAttribute('style', 'position: absolute; top: 25vh; left: 25vw; width: 50vw; height: 50vh; border-radius: 1em; box-shadow: 0 0 .5em rgba(0, 0, 0, .66)');
-            newImg.addEventListener('click', (e) => {
-                e.target.remove();
-            });
+      newImg.className = 'beastify-image';
+      newImg.setAttribute('src', url);
+      newImg.setAttribute('style', 'position: absolute; top: 25vh; left: 25vw; width: 50vw; height: 50vh; border-radius: 1em; box-shadow: 0 0 .5em rgba(0, 0, 0, .66)');
+      newImg.addEventListener('click', (e) => {
+        e.target.remove();
+      });
 
-            Array.from(document.querySelectorAll('.beastify-image')).forEach((img) => {
-                img.remove();
-            });
+      Array.from(document.querySelectorAll('.beastify-image')).forEach((img) => {
+        img.remove();
+      });
 
-            document.body.appendChild(newImg);
-        };
+      document.body.appendChild(newImg);
+    };
 
-    chrome.runtime.onMessage.addListener(handleMessage);
+  chrome.runtime.onMessage.addListener(handleMessage);
+
 })();
--- src/browser-popup.js
+++ src/browser-popup.js
@@ -1,15 +1,17 @@
 /* global $ */
 
 (() => {
-    'use strict';
+  'use strict';
 
-    $('div.beast').on('click', (e) => {
-        const selectedBeast = $('img', e.currentTarget || e.target).attr('src');
+  $('div.beast').on('click', (e) => {
+    const selectedBeast = $('img', e.currentTarget || e.target).attr('src');
 
-        chrome.tabs.executeScript(null, { 'file': '/src/beastify.js' });
+    chrome.tabs.executeScript(null, { 'file': '/src/beastify.js' });
 
-        chrome.tabs.query({ 'active': true, 'currentWindow': true }, (tabs) => {
-            chrome.tabs.sendMessage(tabs[0].id, { 'beast': selectedBeast });
-        });
+    chrome.tabs.query({ 'active': true, 'currentWindow': true }, (tabs) => {
+      chrome.tabs.sendMessage(tabs[0].id, { 'beast': selectedBeast });
     });
+
+  });
+
 })();
