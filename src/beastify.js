a4dc638e6a513bf431854c32e17be56159d73d79
Sudaraka Wijesinghe <sudaraka@sudaraka.org>
2016-01-16 12:51:03 +0530

    Use content script for CSS

--- src/beastify.js
+++ src/beastify.js
@@ -15,7 +15,6 @@
 
       newImg.className = 'beastify-image';
       newImg.setAttribute('src', url);
-      newImg.setAttribute('style', 'position: absolute; top: 25vh; left: 25vw; width: 50vw; height: 50vh; border-radius: 1em; box-shadow: 0 0 .5em rgba(0, 0, 0, .66)');
       newImg.addEventListener('click', (e) => {
         e.target.remove();
       });
